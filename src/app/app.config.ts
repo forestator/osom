b2c4f3a2b8f9a304017b438b87c46c799a37408e
mforet <maximilien.foret@harmonicinc.com>
2024-08-27 22:12:32 +0200

    firebase init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {firebaseConfig} from '../environments/firebase-config';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideRouter(routes)]
 };
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCzY_-2OwD4c7BSGbdHq82t0H_lnyWgcfE",
+    authDomain: "osom-97fed.firebaseapp.com",
+    databaseURL: "https://osom-97fed-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "osom-97fed",
+    storageBucket: "osom-97fed.appspot.com",
+    messagingSenderId: "880648372068",
+    appId: "1:880648372068:web:4587da6920f64e87bac471"
+};
